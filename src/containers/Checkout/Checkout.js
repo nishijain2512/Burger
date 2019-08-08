b31dca627cb4cc474b08bf5a0ac4aef34a076b34
Nishi Jain <nishijain@nishis-mbp.attlocal.net>
2019-08-08 10:23:41 -0400

    implementing redux

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,29 +1,11 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
+import { connect } from 'react-redux';
 //import axios from 'axios';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
-import {Route} from 'react-router-dom';
 import ContactData from '../ContactData/ContactData';
 
 class Checkout extends Component {
-    state = {
-        ingredients: null,
-        totalPrice:0
-    }
-
-    componentWillMount() {
-        const query = new URLSearchParams(this.props.location.search);
-        const ingredients = {};
-        let price = 0;
-        for (let param of query.entries()) {
-            // ['salad', '1']
-            if(param[0] === 'price'){
-                price = param[1];
-            }else {
-                ingredients[param[0]] = +param[1];
-            }
-        }
-        this.setState({ingredients: ingredients, totalPrice: price});
-    }
 
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
@@ -48,4 +30,16 @@ class Checkout extends Component {
 
 }
 
-export default Checkout;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
